75cfe0e3d4884a57acb7b1aa5b2552e2e567837e
Christopher <mr.kitopa@gmail.com>
2020-05-05 18:57:04 +0200

    jhgjg

--- 03LargestPrimeFactor.py
+++ 03LargestPrimeFactor.py
@@ -1,3 +1,6 @@
+import multiprocessing as mp
+
+
 def isprime(zahl):
     output = True
     import math
@@ -7,17 +10,45 @@ def isprime(zahl):
     return output
 
 
-primefactors = [0]
-
-checkzahl = 600851475143
-
-for i in range(3, int(checkzahl), 2):
-    if checkzahl % i == 0:
-        print("Checkzahl:", checkzahl,"Teiler: ", i)
-        if isprime(i):
-            print("Teiler ist prim!")
-        checkzahl = checkzahl/i
+def checkForTeilerIn10erSchrittenVonBis(startZahl, zahl):
+    for i in range(startZahl, zahl, 10):
+        if zahl % i == 0:
+            print("Checkzahl:", zahl, "Teiler: ", i)
+            zahl /= i
+            if isprime(i):
+                print("Teiler ist prim!")
         print("Rest:", checkzahl)
 
 
-print(primefactors)
+def kuerzen(zahl):
+    iseven = True
+    while iseven:
+        if zahl % 2 == 0:
+            zahl /= 2
+        else:
+            iseven = False
+    return int(zahl)
+
+
+def primfaktorZerlegung(zahl):
+    import math
+    primfaktoren = []
+    iseven = True
+    while iseven:
+        if zahl % 2 == 0:
+            primfaktoren.append(2)
+            zahl /= 2
+        else:
+            iseven = False
+
+    for i in range(3, int(zahl) + 1, 2):
+        while zahl % i == 0 and isprime(i):
+            primfaktoren.append(i)
+            zahl /= i
+    return primfaktoren
+
+
+checkzahl = 100876
+
+primFaktoren = primfaktorZerlegung(checkzahl)
+print(primFaktoren)
