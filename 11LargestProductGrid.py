87078c837bd4df4f0f407527863ed3efc0694a4a
Kartoffelkopp <mr.kitopa@gmail.com>
2020-05-08 17:37:01 +0200

    inital commit

--- 11LargestProductGrid.py
+++ 11LargestProductGrid.py
@@ -13,13 +13,34 @@ for x in content:
         zeile.append(int(x[i])*10 + int(x[i+1]))
     grid.append(zeile)
 
-product=0
 matrixGrid = np.array(grid)
+product=0
 #left-right
-# for i in range(0,17):
-#     p = matrixGrid[:,i:i+4]
-#     for zeile in p:
-#         zeilenprod = math.prod(zeile)
-#         print(zeilenprod)
+for j in range(0,17):#spalten
+    for i in range(0,20):#zeilen
+        p = math.prod(matrixGrid[i,j:j+4])
+        if p>product:
+            product=p
 
-test = np.array([[1,2,3],[2,3,4]])
+#oben-unten
+for j in range(0,20):#spalten
+    for i in range(0,17):#zeilen
+        p = math.prod(matrixGrid[i:i+4,j])
+        if p>product:
+            product=p
+
+#diagonal
+for j in range(0,17):#spalten
+    for i in range(0,17):#zeilen
+        p = matrixGrid[i,j]*matrixGrid[i+1,j+1]*matrixGrid[i+2,j+2]*matrixGrid[i+3,j+3]
+        if p>product:
+            product=p
+
+#antidiagonal
+for j in range(0,17):#spalten
+    for i in range(0,17):#zeilen
+        p = matrixGrid[i,j+3]*matrixGrid[i+1,j+2]*matrixGrid[i+2,j+1]*matrixGrid[i+3,j]
+        if p>product:
+            product=p
+
+print(product)
